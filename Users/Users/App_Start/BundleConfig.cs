2ca1d1a9ada2d20b9711d8bc2e002c31ab5ba0e4
Omar Joya <ojoyar@gmail.com>
2021-01-07 12:24:24 -0600

    Add angularjs

--- Users/Users/App_Start/BundleConfig.cs
+++ Users/Users/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace Users
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/AngularCustom").IncludeDirectory(
+                "~/app", "*.js", true));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
